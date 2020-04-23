d6261b11051f8fbb5cf63e0edf4d9367d147e5a0
Bruno <brunormferreira@hotmail.com>
2020-04-23 20:29:37 -0300

    Updates

---
+++ precache-manifest.c68a1425524b06225bd6c4b0132ecbc6.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c03595db5505a2922b4f12ec7e601f43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf093b5cb369989c8cc2",
+    "url": "/static/js/2.86b3d641.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.86b3d641.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0502572635c46b13c8b5",
+    "url": "/static/js/main.c289bafc.chunk.js"
+  },
+  {
+    "revision": "daa2510e2dc590756348",
+    "url": "/static/js/runtime-main.b28561ae.js"
+  },
+  {
+    "revision": "ddac8f7572c556435ab2cb10cbcdf27a",
+    "url": "/static/media/angular.ddac8f75.svg"
+  },
+  {
+    "revision": "0684e470f6cbbbb429d15bb8107dd164",
+    "url": "/static/media/css.0684e470.svg"
+  },
+  {
+    "revision": "a5e61872a683931ab6bcfbdd05c2c9ef",
+    "url": "/static/media/git.a5e61872.svg"
+  },
+  {
+    "revision": "8136d6ce7841b37ea58a29cd402ffdd9",
+    "url": "/static/media/github.8136d6ce.svg"
+  },
+  {
+    "revision": "eef1b72c9de287a8375b293345f3fccb",
+    "url": "/static/media/gitlab.eef1b72c.svg"
+  },
+  {
+    "revision": "f6d74cb1179e07c3a0ae04da63123563",
+    "url": "/static/media/html.f6d74cb1.svg"
+  },
+  {
+    "revision": "a745037d8380b653a8dbb6a16d4c449c",
+    "url": "/static/media/javascript.a745037d.svg"
+  },
+  {
+    "revision": "27c9cccb14e72860e3cb8fba59990b46",
+    "url": "/static/media/profile.27c9cccb.jpg"
+  },
+  {
+    "revision": "7daf3d1eb8906d95edbd49aa834b6938",
+    "url": "/static/media/react.7daf3d1e.svg"
+  },
+  {
+    "revision": "e33f3b82878d5d175b7bcc2576c5d259",
+    "url": "/static/media/vue.e33f3b82.svg"
+  },
+  {
+    "revision": "f5d2d6909b53784b0c984b3c9b7e4ccb",
+    "url": "/static/media/webpack.f5d2d690.svg"
+  }
+]);
