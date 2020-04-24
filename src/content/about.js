575fdfd0ac9687c7d8342ee575630be19d95db13
Bruno <brunormferreira@hotmail.com>
2020-04-24 10:52:36 -0300

    fix content indent

--- src/content/about.js
+++ src/content/about.js
@@ -1,1 +1,22 @@
-export default {"photo":"profile.jpg","name":"Bruno Ramires","bio":"I am currently studying and honing my knowledge of front end technologies. Specifically with HTML, CSS and JavaScript! And some frameworks like React, Vue and Angular.","logotipo":"bruno.rm.ferreira","social":{"github":{"url":"https://github.com/brunormferreira","stats":"+80 Followers","icon":"FiGithub","statsIcon":"FiUserCheck"},"linkedin":{"url":"https://www.linkedin.com/in/bruno-ramires-80395517b","stats":"+200 Connections","icon":"FiLinkedin","statsIcon":"FiStar"},"mail":{"url":"mailto:brunormferreira@hotmail.com","icon":"FiSend"}}}
+export default {
+  photo: "profile.jpg",
+  name: "Bruno Ramires",
+  bio:
+    "I am currently studying and honing my knowledge of front end technologies. Specifically with HTML, CSS and JavaScript! And some frameworks like React, Vue and Angular.",
+  logotipo: "bruno.rm.ferreira",
+  social: {
+    github: {
+      url: "https://github.com/brunormferreira",
+      stats: "+80 Followers",
+      icon: "FiGithub",
+      statsIcon: "FiUserCheck",
+    },
+    linkedin: {
+      url: "https://www.linkedin.com/in/bruno-ramires-80395517b",
+      stats: "+200 Connections",
+      icon: "FiLinkedin",
+      statsIcon: "FiStar",
+    },
+    mail: { url: "mailto:brunormferreira@hotmail.com", icon: "FiSend" },
+  },
+};
